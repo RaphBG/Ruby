548e36e2e4aa504184b8f059b6fd1cbbf45c5900
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-03-25 16:37:27 +0100

    [ADD][MasterMind] Secret Code Generation

---
+++ ruby_projects/6.MasterMind/mastermind.rb
@@ -0,0 +1,23 @@
+class MasterMind
+    @@secret = ""
+    @@used = []
+    
+    def code
+      4.times do
+        number = rand(0..9)
+        if @@used.include?(number)
+          while @@used.include?(number)
+            number = rand(0..9)
+          end  
+        end
+        @@used.push(number)
+        @@secret += number.to_s
+      end
+      puts "Voici le code"
+      puts @@secret
+    end
+end
+
+game = MasterMind.new
+game.code
+
