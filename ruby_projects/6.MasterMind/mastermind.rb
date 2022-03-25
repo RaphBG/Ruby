ddbf2a6d68f2e51660bc6c0547f442315c4c9cb1
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-03-25 16:40:24 +0100

    [FIX][MasterMind] Deleted useless "if"

--- ruby_projects/6.MasterMind/mastermind.rb
+++ ruby_projects/6.MasterMind/mastermind.rb
@@ -1,23 +1,20 @@
 class MasterMind
-    @@secret = ""
-    @@used = []
-    
-    def code
-      4.times do
+  @@secret = ""
+  @@used = []
+  
+  def code
+    4.times do
+      number = rand(0..9)
+      while @@used.include?(number)
         number = rand(0..9)
-        if @@used.include?(number)
-          while @@used.include?(number)
-            number = rand(0..9)
-          end  
-        end
-        @@used.push(number)
-        @@secret += number.to_s
-      end
-      puts "Voici le code"
-      puts @@secret
+      end  
+      @@used.push(number)
+      @@secret += number.to_s
     end
+    puts "Voici le code"
+    puts @@secret
+  end
 end
 
 game = MasterMind.new
 game.code
-
