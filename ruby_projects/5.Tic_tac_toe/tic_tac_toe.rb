f537e3d6afe10787e8df2ab981480d8bbc31ac3f
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-03-25 14:25:49 +0100

    [ADD][Tic-Tac-Toe] Victory Condition + Players

--- ruby_projects/5.Tic_tac_toe/tic_tac_toe.rb
+++ ruby_projects/5.Tic_tac_toe/tic_tac_toe.rb
@@ -1,8 +1,9 @@
-require 'io/console'
 class Game
   @@win = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   @@board = [[1,2,3],[4,5,6],[7,8,9]]
   @@used = []
+  @@player1 = []
+  @@player2 = []
   
   def board
     boardrow = ""
@@ -16,37 +17,53 @@ class Game
     board
     turn=1
     puts "Turn ##{turn}"
+    puts "Player 1 turn!"
+  
     index = gets.to_i
     while turn <= 9
-      index = position_impossible?(index)
-      index = already_used?(index)
+      index = position_impossible(index)
+      index = already_used(index)
       @@used.push(index)
       placing(index, turn)
       turn += 1
+      if player1_win?
+        puts "Player 1 wins!"
+        return
+      end
+      if player2_win?
+        puts "Player 2 wins!"
+        return
+      end
       if turn <= 9
         puts "Turn ##{turn}"
+        if turn.odd?
+          puts "Player 1 turn!"
+        else
+          puts "Player 2 turn!"
+        end
         index = gets.to_i
       end
     end
+    puts "No one win !"
   end
 
-  def position_impossible?(index)
+  def position_impossible(index)
     if(index.between?(0,9) == false)
       while index.between?(0,9) == false
-        puts "this position doesn't exist"
+        puts "This position doesn't exist"
         index = gets.to_i
       end  
     end
     return index
   end
 
-  def already_used?(index)
+  def already_used(index)
     if(@@used.include?(index))
       while @@used.include?(index) || index.between?(0,9) == false
         if(index.between?(0,9) == false)
-          puts "this position doesn't exist"
+          puts "This position doesn't exist"
         else
-          puts "this position has already been played"
+          puts "This position has already been played"
         end
         index = gets.to_i
       end
@@ -60,13 +77,51 @@ class Game
         if position == index
           if turn.odd?
             @@board[index_r][index_c] = "x"
+            @@player1.push(index)
           else
             @@board[index_r][index_c] = "o"
+            @@player2.push(index)
           end
           board
         end
       end
     end
+  end
+
+  def player1_win?
+    count=0
+      @@win.each_with_index do |data, index_r|
+        data.each_with_index do |position, index_c|
+          @@player1.each_with_index do |choice, index|
+            if position == choice
+              count +=1
+            end
+            if count == 3
+              return true
+            end
+          end
+        end
+      count=0
+    end
+    return false
+  end
+
+  def player2_win?
+    count=0
+      @@win.each_with_index do |data, index_r|
+        data.each_with_index do |position, index_c|
+          @@player2.each_with_index do |choice, index|
+            if position == choice
+              count +=1
+            end
+            if count == 3
+              return true
+            end
+          end
+        end
+      count=0
+    end
+    return false
   end
   
 end
