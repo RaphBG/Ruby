ffdf0bc6a2f781a4752ab88b939e2ff5807490cb
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-03-29 16:04:59 +0200

    [ADD][Hangman] Save + load save

--- ruby_projects/8.hangman/hangman.rb
+++ ruby_projects/8.hangman/hangman.rb
@@ -1,15 +1,15 @@
+require 'yaml'
+
 class Hangman
     
     def initialize
-        @secret_word = ""
-        @miss = []
         @try = 10
+        @miss = []
         words = File.readlines('words.txt')
         number = rand(0..9894)
 
         if words[number].chomp.length.between?(5,12)
             @secret_word = words[number].chomp
-            puts @secret_word
         elsif !words[number].chomp.length.between?(5,12)
             initialize
         end
@@ -20,24 +20,27 @@ class Hangman
         end
     end
 
-    def game
+    def game(game)
+        game = game
         while @try !=0 && !win?
+            save(game)
             puts "Guess: #{@hidden.join(' ')}"
             puts "Miss: #{@miss.join(',')}"
             choice
         end
-        puts "The word was #{@secret_word}."
+        puts "The word was #{@secret_word} !"
         if win?
             puts "Good Job !"
         else
             puts "You failed"
         end
+        File.delete("saved/save.yaml") if File.exist?("saved/save.yaml")
     end
 
     def choice 
         puts "Write a downcase letter"
         player_choice=gets.chomp
-        while player_choice.ord.between?(97,122) != false && player_choice.length != 1
+        while !player_choice.ord.between?(97,122) && player_choice.length != 1
             puts "Write a downcase letter"
             player_choice = gets.chomp
         end
@@ -60,8 +63,29 @@ class Hangman
     def win?
         @hidden.join('') == @secret_word
     end
+
+    def save(game)
+        Dir.mkdir('saved') unless Dir.exist?('saved')
+        filename = "save"
+        return false unless filename
+        dump = YAML.dump(game)
+        File.open(File.join(Dir.pwd, "saved/save.yaml"), 'w') { |file| file.write dump }
+    end
     
+    def load
+        if File.exist?("saved/save.yaml")
+            saved = File.open(File.join(Dir.pwd, "saved/save.yaml"), 'r')
+            loaded_game = YAML.load(saved)
+            saved.close
+            puts "Your previous saved game has been loaded."
+            loaded_game
+        else
+            Hangman.new   
+        end
+    end
+
 end
 
 game = Hangman.new
-game.game
+game = game.load
+game.game(game)
