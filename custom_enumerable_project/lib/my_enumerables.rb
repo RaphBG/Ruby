6eb538e454abe2be3fcd4709a00cfe4feac9b596
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-04-04 11:30:59 +0200

    [ADD][My_each] Recode of each method

---
+++ custom_enumerable_project/lib/my_enumerables.rb
@@ -0,0 +1,16 @@
+module Enumerable
+  # Your code goes here
+end
+
+# You will first have to define my_each
+# on the Array class. Methods defined in
+# your enumerable module will have access
+# to this method
+class Array
+  # Define my_each here
+  def my_each
+    for element in self do
+      yield element
+    end
+  end
+end
