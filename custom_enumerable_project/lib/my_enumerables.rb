a378a5187f545c55dcf870f22d7f75ad34e0912b
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-04-04 11:34:12 +0200

    [ADD][My_each_with_index] Recode of each_with_index

--- custom_enumerable_project/lib/my_enumerables.rb
+++ custom_enumerable_project/lib/my_enumerables.rb
@@ -1,5 +1,13 @@
 module Enumerable
   # Your code goes here
+  
+  def my_each_with_index
+    index = 0
+    for element in self do
+      yield element, index
+      index+=1
+    end
+  end
 end
 
 # You will first have to define my_each
