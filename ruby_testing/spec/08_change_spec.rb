f3c5005d935cf45558bcb623531419432ccde9ca
RaphBG <92065369+RaphBG@users.noreply.github.com>
2022-04-05 11:44:37 +0200

    [ADD][spec 08_change]

--- ruby_testing/spec/08_change_spec.rb
+++ ruby_testing/spec/08_change_spec.rb
@@ -49,21 +49,26 @@ end
 # ASSIGNMENT:
 
 describe 'lucky numbers with rotate! method' do
-  subject(:lucky_numbers) { [3, 7, 13, 31, 42] }
+  
   # Write a test that expresses each of the following statements.
   # Use the array #rotate! method to manipulate lucky_numbers.
 
   context 'when rotating the array of lucky numbers' do
+    subject(:lucky_numbers) { [3, 7, 13, 31, 42] }
+    
     # remove the 'x' before running this test
-    xit 'will change the first value to 7' do
+    it 'will change the first value to 7' do
+      expect { lucky_numbers.rotate!(1) }.to change { lucky_numbers.first }.to(7)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the last value to 3' do
+    it 'will change the last value to 3' do
+      expect { lucky_numbers.rotate!(1) }.to change { lucky_numbers.last }.to(3)
     end
 
     # remove the 'x' before running this test
-    xit 'will change the first value to 7 and last value to 3' do
+    it 'will change the first value to 7 and last value to 3' do
+      expect { lucky_numbers.rotate!(1) }.to change { lucky_numbers.first }.to(7).and change { lucky_numbers.last }.to(3)
     end
   end
 end
