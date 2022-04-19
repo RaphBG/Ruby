cb31c5a7bdf91a03c8a4b8f5f2338f7283c987db
berton_r <raphael@LAPTOP-LU69M3NP.localdomain>
2022-04-19 17:03:52 +0200

    initial commit

--- rubyonrails/micro-reddit/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { in: 4..12 }
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { in: 6..16 }
-end
