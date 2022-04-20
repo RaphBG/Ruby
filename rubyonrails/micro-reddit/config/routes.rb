26cac471285140f35493b544a2b113a045d2df3b
berton_r <raphael@LAPTOP-LU69M3NP.localdomain>
2022-04-20 16:29:20 +0200

    Users functions OK + adding comments to posts

--- rubyonrails/micro-reddit/config/routes.rb
+++ rubyonrails/micro-reddit/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  devise_for :users, :controllers =>{registrations: 'registrations'}
+  resources :users
   root "posts#index"
+  resources :posts do
+    resources :comments
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- rubyonrails/micro-reddit/db/schema.rb
+++ rubyonrails/micro-reddit/db/schema.rb
@@ -10,13 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_153720) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_134904) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "resume"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
