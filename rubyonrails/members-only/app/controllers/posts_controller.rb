5696fdc7e462193f99541fe8adb139300aaa5a0a
berton_r <raphael@LAPTOP-LU69M3NP.localdomain>
2022-04-22 15:35:19 +0200

    members-only finished

---
+++ rubyonrails/members-only/app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        redirect_to root_path, notice: "Post was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def post_params
+      params.require(:post).permit(:body, :user_id)
+    end
+end
