08ac823c83e52649443a1e9127d06f4c654c2417
berton_r <raphael@LAPTOP-LU69M3NP.localdomain>
2022-04-28 10:38:26 +0200

    Choosing flight finished

--- rubyonrails/fligth-booker/app/controllers/flights_controller.rb
+++ rubyonrails/fligth-booker/app/controllers/flights_controller.rb
@@ -2,5 +2,9 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
     @date_options = Flight.all.map { |flight| flight.scheduled_on.to_date }.uniq
+    if params.has_key?(:date)
+      @selected_date = params[:date].to_date
+      @available_flights = Flight.where(from_airport_id: params[:from_airport_id], to_airport_id: params[:to_airport_id], scheduled_on: @selected_date.all_day)
+    end
   end
 end
